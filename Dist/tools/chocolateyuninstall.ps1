3cb622373fa008e90b3f455d7a5396c9c794b46d
Alexander Steinhöfer <kontakt@lx-s.de>
2022-04-12 16:01:04 +0200

    Updated chocolatey stuff.

--- Dist/tools/chocolateyuninstall.ps1
+++ Dist/tools/chocolateyuninstall.ps1
@@ -1,2 +1,3 @@
 ﻿Stop-Process -Name WinMute
-Remove-Item HKCU:\Software\lx-systems\WinMute
+Remove-Item "HKCU:\SOFTWARE\lx-systems\WinMute"
+Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "LX-Systems WinMute"
